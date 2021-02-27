b37a5efd156ad4c03c5514e140f7b0e0c6923b78
Kelly <Kelly@mac-pro.local>
2021-02-27 00:50:02 +0000

    initial commit, not tested, not complete yet

---
+++ Scripts/main.js
@@ -0,0 +1,25 @@
+const mivaSnippets = require("./snippets/snippets.json");
+
+class CompletionProvider {
+  provideCompletionItems(editor, context) {
+    let snippets = [];
+
+    for (let snippet in mivaSnippets) {
+      let item = new CompletionItem(
+        mivaSnippets[snippet].prefix,
+        CompletionItemKind.File
+      );
+      console.log(mivaSnippets[snippet].description);
+
+      item.documentation = mivaSnippets[snippet].description;
+      item.insertText = mivaSnippets[snippet].body;
+      item.tokenize = true;
+
+      snippets.push(item);
+    }
+
+    return snippets;
+  }
+}
+
+nova.assistants.registerCompletionAssistant("miva", new CompletionProvider());
